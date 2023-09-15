3010e135fbe29f795a3ea7f1df1599761ef604af
Vishal Nivangune <95202004+VishalAnna@users.noreply.github.com>
2023-09-15 23:59:21 +0530

    Update streamlit_app.py
    added try and except block

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,19 +24,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-# import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-
-
-# Return the json data in normalize way which we can read 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output to the screen as a readbale table form
-streamlit.dataframe(fruityvice_normalized)
-# don't run after this command until i call to 
-streamlit.stop()
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.stop()
 
 
 # import snowflake.connector
