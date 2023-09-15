c1fe66a3aa609556335e189b17e69f0a349cc98a
Vishal Nivangune <95202004+VishalAnna@users.noreply.github.com>
2023-09-15 23:52:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-import urllib.error 
+from urllib.error import URLError 
 streamlit.title('My Moms New healthy dinner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueburry oatmeal')
