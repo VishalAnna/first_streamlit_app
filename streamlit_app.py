d43567b1b1e8282f985f5d884143708585151ac5
Vishal Nivangune <95202004+VishalAnna@users.noreply.github.com>
2023-09-16 11:39:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ streamlit.header("The Fruit load list conatin")
 # import snowflake.connector
 # snowflake related function
 def get_fruit_load_list():
-  with my_cnx.cursor()=my_cur:
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("Select * from fruit_load_list")
     return my_cur.fetchall()
     
